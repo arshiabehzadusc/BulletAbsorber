a5618c726784bbd8a21fbe195989df455e535d37
arshiabehzad <behzad@usc.edu>
2023-06-19 11:58:38 -0700

    Picking up Items + Super Basic (console based) Inventory System
    - Item Prefab Made
    - Can pick up item
    - Can add item to list (inventory) prints to console

--- OdditiesProject/Assets/Scripts/PlayerController.cs
+++ OdditiesProject/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     public float moveSpeed = 5f;
+
+    private List<string> inventory = new List<string>();
+
+    private bool pickUp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickUp = false;
     }
 
     // Update is called once per frame
@@ -19,5 +24,30 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = moveSpeed * Time.deltaTime * new Vector3(horizontalInput, verticalInput, 0f);
         transform.position += movement;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pickUp = true;
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // Code to execute when a trigger collision starts
+        if (pickUp)
+        {
+            Debug.Log("Pick up: " + other.gameObject.name);
+            inventory.Add(other.gameObject.name);
+            print("Inventory: ");
+            foreach (string item in inventory)
+            {
+                print(item+ ", ");
+            }
+            pickUp = false;
+            Destroy(other.gameObject);
+        }
     }
 }
+
+
+    
